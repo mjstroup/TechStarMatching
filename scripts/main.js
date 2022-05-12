0f6431b86cd7ef4235a1902839dcce0e5ab39a8a
Matthew Stroup <mjstroup@purdue.edu>
2022-05-12 10:18:53 -0500

    results page and updated js

--- scripts/main.js
+++ scripts/main.js
@@ -10,59 +10,42 @@ function addUserData(data) {
     userData.push(data);
 }
 
+/**
+ * Calculates pair-wise similarities between all users
+ * Returns a matrix of similarities
+ */
 function calculateSimilarity() {
     //Format of data array is that first two elements are firstName lastName respectively
     const names = userData.map(subarray => (subarray.splice(0, 2))); //Last index is non inclusive!
     const datas = userData.map(subarray => (subarray.splice(2)));
-    
+
     //Similarity matrix
-    similarity = []
-    for(let i = 0; i < userData.length; i++) {
-        for(let j = 0; j < userData.length; j++) {
-            
-
-
-        }
+    const similarity = [];
+    for (let i = 0; i < userData.length; i++) {
+        const row = [];
+        userData.forEach(element => row.append(calculateEucSim(userData[i].splice(2), userData[j].splice(2))));
+        similarity.append(row);
     }
 
-    
+    return similarity;
 }
 
 //receive from survey.html
 //https://developer.mozilla.org/en-US/docs/Learn/Forms/Sending_forms_through_JavaScript
+function getData() {
+    var first_name = document.getElementById('firstname').value;
+    var last_name = document.getElementById('lastname').value;
 
-var first_name = document.getElementById('firstname');
-var last_name = document.getElementById('lastname');
+    data = [first_name, last_name];
+    for(let i = 1; i <= 20; i++) {
+        data.push(document.getElementById('q' + String(i)).value);
+    }
+    console.log(data);
+    alert(data);
 
-var q1_slider_value = document.getElementById('q1');
-var q2_slider_value = document.getElementById('q2');
-var q3_slider_value = document.getElementById('q3');
-var q4_slider_value = document.getElementById('q4');
-var q5_slider_value = document.getElementById('q5');
-var q6_slider_value = document.getElementById('q6');
-var q7_slider_value = document.getElementById('q7');
-var q8_slider_value = document.getElementById('q8');
-var q9_slider_value = document.getElementById('q9');
-var q10_slider_value = document.getElementById('q10');
-var q11_slider_value = document.getElementById('q11');
-var q12_slider_value = document.getElementById('q12');
-var q13_slider_value = document.getElementById('q13');
-var q14_slider_value = document.getElementById('q14');
-var q15_slider_value = document.getElementById('q15');
-
-
-
-/*
-code that can be used but this is really
-more for my sanity than anything else
- 
-const names = [];
-
-var name = first_name + last_name;
-
-names[names.length] = name;
-
-*/
+    //addUserData(data);
+    alert(userData);
+}
 
 
 
